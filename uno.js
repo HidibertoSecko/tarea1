58f51b80689cab352618730dd01b111d5aed92df
HidibertoSecko <hidibertosecko@gmail.com>
2019-04-05 00:39:06 -0400

    ejercicio uno arreglado

--- uno.js
+++ uno.js
@@ -1,14 +1,17 @@
 function uno()
 {
-  var matrix = [[0, 1, 1, 2],
-                [0, 5, 0, 0],
-                [2, 0, 3, 3]];
+  var matrix = [[1, 1, 0, 0],
+                [0, 5, 4, 1],
+                [2, 1, 3, 10]];
   var suma = 0;
   for (var i = 0; i < 3; i++){
     for (var j = 0; j < 4; j++){
       if(i>=1){
         if(matrix[i-1][j] != 0)
           suma = suma + matrix[i][j];
+          else {
+            matrix[i][j]=0;
+          }
       }
       else
         suma = suma + matrix[i][j];
